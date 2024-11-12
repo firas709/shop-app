032ea3453a36a17f45f1ed7296401e85a2cdc000
firashkimi <firashkimi09@gmail.com>
2024-11-12 17:54:30 +0100

    setup  ci/cd pipeline for customer service

---
+++ services/customer/src/main/java/tn/firas/customer/CustomerApplication.java
@@ -0,0 +1,20 @@
+package tn.firas.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CustomerApplication {
+	@GetMapping
+	public String hello(){
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+}
