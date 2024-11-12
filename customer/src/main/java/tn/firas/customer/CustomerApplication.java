84d2d256b3253affbaeb6f24d024a67c74bb889f
firashkimi <firashkimi09@gmail.com>
2024-11-12 18:54:55 +0100

    fix customer.yml version 7

--- customer/src/main/java/tn/firas/customer/CustomerApplication.java
+++ customer/src/main/java/tn/firas/customer/CustomerApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerApplication {
 	@GetMapping
 	public String hello(){
-		return "Hello World";
+		return "Hello World !";
 	}
 
 	public static void main(String[] args) {
